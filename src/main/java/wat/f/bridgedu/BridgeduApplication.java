09d49dfd229d6b521a249476dc7b98e4fd0f31de
suzuki-hirokazu-wingarc <suzuki.hir@wingarc.com>
2023-06-22 17:44:10 +0900

    Spring Init

---
+++ src/main/java/wat/f/bridgedu/BridgeduApplication.java
@@ -0,0 +1,13 @@
+package wat.f.bridgedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridgeduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wat/f/bridgedu/BridgeduApplicationTests.java
@@ -0,0 +1,13 @@
+package wat.f.bridgedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
