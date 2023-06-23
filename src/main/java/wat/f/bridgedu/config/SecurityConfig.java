60f220028ee8d809c9cc39b804a8f469e1afe467
suzuki-hirokazu-wingarc <133177576+suzuki-hirokazu-wingarc@users.noreply.github.com>
2023-06-23 10:52:11 +0900

    ログインまで

---
+++ src/main/java/wat/f/bridgedu/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package wat.f.bridgedu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeRequests(auth -> auth.antMatchers("/login**").permitAll())
+            .authorizeRequests(auth -> auth.antMatchers("/h2-console/**").hasRole("ADMIN"))
+            .csrf(auth -> auth.ignoringAntMatchers("/h2-console/**"))
+            .headers(auth -> auth.frameOptions(frame -> frame.sameOrigin()))
+            .authorizeRequests(auth -> auth.anyRequest().hasRole("USER"))
+            .formLogin(auth -> auth.loginPage("/login")
+            .usernameParameter("username").passwordParameter("password"))
+            .build();
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+drop table if exists milestones;
+
+create table milestones (
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userid BIGINT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    memo VARCHAR(255) NOT NULL,
+    achievement TINYINT NOT NULL,
+    goal DATE NOT NULL,
+    created DATE NOT NULL,
+    modified DATE NOT NULL
+);
